27b1ee165b0514670e40da4de93791020d74f4d7
Grzegorz Chrupała <g.chrupala@uvt.nl>
2016-02-04 14:31:11 +0100

    data

---
+++ data/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://cs.stanford.edu/people/karpathy/deepimagesent/flickr8k.zip
+wget http://cs.stanford.edu/people/karpathy/deepimagesent/flickr30k.zip
+wget http://cs.stanford.edu/people/karpathy/deepimagesent/coco.zip
+
