030d1ee831885509909c73de97965a29da9c19ec
Grzegorz Chrupała <g.chrupala@uvt.nl>
2016-02-04 15:11:18 +0100

    Added simmple example.

---
+++ simple_example.py
@@ -0,0 +1,29 @@
+import json
+from imaginet.commands import train, evaluate, compressed
+from funktional.util import linear, clipped_rectify, CosineDistance
+dataset = 'flickr8k'
+epochs = 1
+
+
+train(dataset=dataset,
+      datapath='/home/gchrupala/repos/neuraltalk/',
+      model_path='.',
+      epochs=epochs,
+      min_df=10,
+      max_norm=5.0,
+      scale=True,
+      batch_size=64,
+      shuffle=True,
+      size_embed=256,
+      size_hidden=1024,
+      depth=3,
+      tokenize=compressed,
+      validate_period=64*1000)
+
+for epoch in range(1, epochs+1):
+    
+    scores = evaluate(dataset=dataset,
+                      datapath='/home/gchrupala/repos/neuraltalk/',
+                      model_path='.',
+                      model_name='model.{}.pkl.gz'.format(epoch))
+    json.dump(scores, open('scores.{}.json'.format(epoch),'w'))
