95cc0c7ee5e684ca3cf95e775dfd84e881f1711b
Grzegorz Chrupała <pitekus@gmail.com>
2016-07-27 14:25:56 +0200

    scores.py

--- scores.py
+++ scores.py
@@ -2,11 +2,14 @@ import numpy
 import json
 
 print "epoch r@1 R@5 r@10 rank"
-for i in range(1,11):
-    data = json.load(open("scores.{}.json".format(i)))
-    print i, numpy.mean(data['recall']['1']), \
-             numpy.mean(data['recall']['5']),\
-             numpy.mean(data['recall']['10']),\
-             numpy.median(data['ranks'])
-    
+for i in range(1,21):
+    try:
+        data = json.load(open("scores.{}.json".format(i)))
+        print i, numpy.mean(data['recall']['1']), \
+                 numpy.mean(data['recall']['5']),\
+                 numpy.mean(data['recall']['10']),\
+                 numpy.median(data['ranks'])
+    except IOError:
+        pass
+
     
