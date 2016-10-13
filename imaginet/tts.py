0e97d953e9f633cf7ad3a3e1826106dac833202a
Grzegorz Chrupała <pitekus@gmail.com>
2016-10-13 18:00:01 +0200

    Example code for synthesizing speech and extracting mfcc features.

--- imaginet/tts.py
+++ imaginet/tts.py
@@ -4,14 +4,16 @@ from gtts import gTTS
 import StringIO
 import base64
 import pydub
-import features
+import python_audio_features as features
 import StringIO
 import scipy.io.wavfile as wav
 import numpy
-import features
 import time
 from urllib2 import HTTPError
 import funktional.util as util
+import numpy.random
+import random
+import os
 
 def speak(words):
     f = StringIO.StringIO()
@@ -64,24 +66,48 @@ def extract_fbank(sound):
     fbank_feat = features.logfbank(sig,rate)
     return fbank_feat
 
-def featurefile(dataset='flickr8k', chunksize=1000, kind='fbank'):
+def featurefile(dataset='flickr8k', chunksize=1000, kind='fbank', noisy=False):
     if kind == 'mfcc':
         extract = extract_mfcc
     elif kind == 'fbank':
         extract = extract_fbank
     else:
         raise "Invalid kind"
-    for i,chunk in enumerate(util.grouper(gzip.open("/home/gchrupala/repos/reimaginet/data/{}/dataset.mp3.jsonl.gz".format(dataset)),chunksize)):
+    infix = ".noisy" if noisy else ""
+    for i,chunk in enumerate(util.grouper(gzip.open("/home/gchrupala/repos/reimaginet/data/{}/dataset{}.mp3.jsonl.gz".format(dataset, infix)),chunksize)):
         result = []
         for line in chunk:
             sent = json.loads(line)
             sound = decodemp3(base64.b64decode(sent['speech']))
             result.append(extract(sound))
-        numpy.save("/home/gchrupala/repos/reimaginet/data/{}/dataset.{}.{}.npy".format(dataset,kind,i), result)
+        numpy.save("/home/gchrupala/repos/reimaginet/data/{}/dataset{}.{}.{}.npy".format(dataset,infix,kind,i), result)
 
+import pydub.playback
+
+def noisyspeech(dataset='flickr8k'):
+    dir = "/home/gchrupala/repos/reimaginet/data/PCCdata16kHz/train/background/"
+    background = sum((pydub.AudioSegment.from_mp3(dir + file) for file in os.listdir(dir)[:10]
+                      if file.endswith(".wav")), pydub.AudioSegment.empty())
+    print "Background noise loaded"
+    with gzip.open("/home/gchrupala/repos/reimaginet/data/{}/dataset.noisy.mp3.jsonl.gz".format(dataset),"w") as out:
+        for line in gzip.open("/home/gchrupala/repos/reimaginet/data/{}/dataset.mp3.jsonl.gz".format(dataset)):
+            sent = json.loads(line)
+            print sent['sentid']
+            sound = from_mp3(base64.b64decode(sent['speech']))
+#            pydub.playback.play(sound)
+            noisy = noisify(sound, background)
+#             pydub.playback.play(noisy)
+            io = StringIO.StringIO()
+            noisy.export(io, format='mp3')
+            sent['speech'] = base64.b64encode(io.getvalue())
+            out.write(json.dumps(sent))
+            out.write("\n")
+        
 def noisify(sound, noise):
     loudness = random.uniform(0.0, 10.0)
-    start = random.randint(0, (noise.duration_seconds - sound.duration_seconds) * 1000)
-    speed = random.uniform(1.0, 1.3)
-    sound.overlay(noise[start:].speedup(playback_speed=speed) + loundness)
+    start = random.randint(0, int((noise.duration_seconds - sound.duration_seconds) * 1000))
+    speed = 1 + abs(numpy.random.normal(0.0, 0.1))
+    noisy = sound.speedup(playback_speed=speed).overlay(noise[start:] + loudness)
+    return noisy
+
     
